984ab13eaa28935c16ca5f348955b486934b81c3
jvolcy <jerry.volcy@gmail.com>
2018-06-19 16:27:09 -0400

    UI Changes

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,19 @@
 package sample;
 
+import javafx.scene.control.Button;
+import javafx.scene.web.WebView;
+
 public class Controller {
+
+    public WebView wvOutput;
+    public Button btnPrev;
+    public Button btnNext;
+
+    public void btnPrevClick()
+    {
+        System.out.println("'Prev' button clicked.");
+        wvOutput.getEngine().load("file:///Users/jvolcy/work/Spelman/Projects/data/AGP0202.html");
+    }
+
+
 }
