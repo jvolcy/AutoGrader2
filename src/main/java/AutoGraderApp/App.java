97fba3b741a7b69f171429318ab5eab07c332e9a
jvolcy <jerry.volcy@gmail.com>
2020-06-18 11:27:03 -0400

    AG2 GUI launches with mock Controller.

--- src/main/java/AutoGraderApp/App.java
+++ src/main/java/AutoGraderApp/App.java
@@ -16,17 +16,38 @@ public class App extends Application {
     private static Scene scene;
 
     @Override
-    public void start(Stage stage) throws IOException {
+    public void start(Stage primaryStage) throws IOException {
+        /*** works
         scene = new Scene(loadFXML("primary"), 640, 480);
         stage.setScene(scene);
         stage.show();
+         ****/
+
+
+        Parent root = FXMLLoader.load(getClass().getResource("AutoGraderApp.fxml"));
+        //Parent root = FXMLLoader.load(getClass().getResource("primary.fxml"));
+        //Parent root = loadFXML("primary");
+        //Parent root = loader.load();
+
+        // Get the Controller from the FXMLLoader
+        //Controller controller = loader.getController();
+        //controller.setGradingEngine(autoGrader.getGradingEngine());
+        //controller.setAutoGraderRef(autoGrader);
+
+        //primaryStage.setTitle(appName);
+        primaryStage.setScene(new Scene(root, 640, 480));
+        //primaryStage.setMinWidth(MIN_STAGE_WIDTH);
+        //primaryStage.setMinHeight(MIN_STAGE_HEIGHT);
+
+        //primaryStage.getIcons().add(new Image("/ag2_icon.png"));
+        primaryStage.show();
     }
 
     static void setRoot(String fxml) throws IOException {
-        scene.setRoot(loadFXML(fxml));
+        scene.setRoot(xloadFXML(fxml));
     }
 
-    private static Parent loadFXML(String fxml) throws IOException {
+    private static Parent xloadFXML(String fxml) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource(fxml + ".fxml"));
         return fxmlLoader.load();
     }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module AutoGraderApp {
     requires javafx.controls;
     requires javafx.fxml;
+    requires javafx.web;
 
     opens AutoGraderApp to javafx.fxml;
     exports AutoGraderApp;
