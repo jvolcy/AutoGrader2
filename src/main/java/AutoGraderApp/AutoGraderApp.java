a95b4cd173c5aa80cecff1a2249742ba0de5fba0
jvolcy <jerry.volcy@gmail.com>
2020-06-18 11:31:10 -0400

    Renamed App to AutoGraderApp

--- src/main/java/AutoGraderApp/AutoGraderApp.java
+++ src/main/java/AutoGraderApp/AutoGraderApp.java
@@ -11,7 +11,7 @@ import java.io.IOException;
 /**
  * JavaFX App
  */
-public class App extends Application {
+public class AutoGraderApp extends Application {
 
     private static Scene scene;
 
@@ -48,7 +48,7 @@ public class App extends Application {
     }
 
     private static Parent xloadFXML(String fxml) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource(fxml + ".fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(AutoGraderApp.class.getResource(fxml + ".fxml"));
         return fxmlLoader.load();
     }
 
